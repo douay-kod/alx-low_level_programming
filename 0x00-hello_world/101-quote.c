3c94d9dfb48ad079590525ee42cd240327d729df
douay-kod <douay.youssef@outlook.com>
2023-02-20 15:51:22 -0800

    eee

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - a function that prints
+ * Return: 0(s)
+ */
+
+int main(void)
+{
+	 write(2,"and that piece of art is useful\" -Dora Kopar, 2015-10-19\n",59);
+	 return (1);
+}
