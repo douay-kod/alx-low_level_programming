ec1ee4d4184a528b919292a232384b6fd1262884
douay-kod <douay.youssef@outlook.com>
2023-02-20 17:36:31 -0800

    ee

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - functions
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0m m < 100; m++)
+		{
+			putchar((n / 10) + 48);
+			putchar((n % 10) + 48);
+			putchar(' ');
+			putchar((m / 10) + 48);
+			putchar((m % 10) + 48);
+			if (n != 98 || m != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
