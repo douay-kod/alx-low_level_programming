9b721360742aed63f04a2bc8b248dca5420e85ad
douay-kod <douay.youssef@outlook.com>
2023-02-20 16:49:42 -0800

    ee

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - functions
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
